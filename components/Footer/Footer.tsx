0457845d186ebd0fc13f7c3492927d5f4ee63ccf
Muhammad Faizan Soomro <141871174+Faizan418@users.noreply.github.com>
2024-10-31 00:45:42 +0500

    update

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,14 +6,14 @@ function Footer() {
   return (
     <div className={styles.containar}>
         <div className={styles.one}>
-            <p>Muhammad Faizan --- 2024</p>
+            <p>Muhammad Faizan 2024</p>
         </div>
         <div className={styles.two}>
             <ul className={styles.ul_li}>
-                <li className={styles.li}><Link href={"#"}>Linkedin</Link></li>
-                <li className={styles.li}><Link href={"#"}>Whatsapp</Link></li>
-                <li className={styles.li}><Link href={"#"}>Twitter</Link></li>
-                <li className={styles.li}><Link href={"#"}>Instagram</Link></li>
+                <Link href={"https://www.linkedin.com/in/muhammad-faizan-2541132b8"}><li className={styles.li}>Linkedin</li></Link>
+                <Link href={"https://wa.me/+923406044359"}><li className={styles.li}>Whatsapp</li></Link>
+                <Link href={"https://x.com/muhammadf4060"}><li className={styles.li}>Twitter</li></Link>
+                <Link href={"https://www.instagram.com/fazii.963/"}><li className={styles.li}>Instagram</li></Link>
             </ul>
         </div>
       
