a5bf478e789aa1f15b49763bcf3e31f38409f232
Muhammad Faizan Soomro <141871174+Faizan418@users.noreply.github.com>
2024-10-30 17:42:43 +0500

    update

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,8 +6,8 @@ function Header() {
   return (
     <div className={styles.containar}>
         <div className={styles.first}>
-            <div className={styles.one}>Hello,  I'm <br /> Muhammad Faizan.</div>
-            <div className={styles.two}>I'm student of Governor sindh IT initiative Cloud Computing Genrative AI. & Saylani Mass IT Training Student.</div>
+            <div className={styles.one}>Hello,  I&rsquo;m <br /> Muhammad Faizan.</div>
+            <div className={styles.two}>I&rsquo;m student of Governor sindh IT initiative Cloud Computing Genrative AI. & Saylani Mass IT Training Student.</div>
         </div>
     </div>
   )
